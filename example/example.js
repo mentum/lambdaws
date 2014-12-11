b6c20f6cca2dad7a5bbbe7a4ad5e033054b07c14
francois-xavier <frxpoulin@gmail.com>
2014-12-11 15:43:21 -0500

    refactor sqs stuff

---
+++ example/example.js
@@ -0,0 +1,31 @@
+var lambdaws = require('../'),
+	λ = lambdaws.create;
+
+lambdaws.config({
+	accessKey: 'AKIAINF2FWKX3LDRTR6Q',
+	secretKey: 'LupbZECzd+qC3Y7GVtWQHoNWgraWTfqOYvTc1NNY',
+	role: 'arn:aws:iam::246175073899:role/lambda_exec_sqs',
+	region: 'us-east-1'
+});
+
+var calculator = function(a, b, callback) {
+	callback(a + b);
+};
+
+var cloudedCalculator = λ(calculator, {
+	name: 'CALCULATOR'
+});
+
+lambdaws.start();
+
+// var stdin = process.openStdin();
+
+// stdin.on('data', function(chunk) {
+	
+// 	cloudedCalculator(2, 6, function(data) {
+// 		console.log("<--", data)
+// 	});
+
+// });
+
+setTimeout(function() {}, 1000 * 1000); // Keep Alive
